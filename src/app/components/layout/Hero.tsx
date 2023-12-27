5697b7c9dc9c087bcf3340fb475841c14319672f
George Lugo <machinelearner2334@gmail.com>
2023-12-26 20:00:10 -0800

    Update Hero.tsx
    Changed colors for a better feel

--- src/app/components/layout/Hero.tsx
+++ src/app/components/layout/Hero.tsx
@@ -10,13 +10,13 @@ const Hero: React.FC = () => {
     });
 
     return (
-        <section className={`bg-gray-900 text-white py-16 text-center rounded-md`}>
+        <section className={`bg-zinc-900 text-white py-16 text-center rounded-md`}>
             <animated.div style={fadeIn} className={`max-w-2xl mx-auto`}>
                 <h1 className={`text-4xl font-bold mb-4`}>Welcome to RealmLink</h1>
                 <p className={`mb-2`}>Where your virtual adventures transcend boundaries!</p>
                 <p className={`mb-2`}>With RealmLink, carry your characters, items, and pets across multiple gaming realms.</p>
                 <p className={`mb-6`}>Unleash the power of web3 technology and weave your digital legacy through interconnected games.</p>
-                <button className={`bg-blue-500 text-white py-2 px-4 rounded-full text-lg cursor-pointer mt-6`}>Start Your Journey</button>
+                <button className={`bg-blue-800 text-white py-2 px-4 rounded-full text-lg cursor-pointer mt-6`}>Start Your Journey</button>
             </animated.div>
         </section>
     )
