374a0e2827d2ef4b216d0fa92830bceca9d17a96
Jonny <Email@Email.com>
2024-01-04 13:01:33 +0000

    changin data

--- src/app/components/data/developersData.ts
+++ src/app/components/data/developersData.ts
@@ -3,6 +3,6 @@ export const developersData = [
     { name: "Dave Ring", title: "Senior Blockchain Developer", photo: "/Dave.png", description: ""},
     { name: "Rafiq Whitley", title: "Senior Backend Developer", photo: "/Rafiq.jpg", description: ""},
     { name: "Ayush Agrawal", title: "Node Developer", photo: "/Ayush.jpg", description: "Ayush Agrawal, Backend Specialist, MERN Developer, TypeScript, Express.js, Node.js, DynamoDB, Serverless Framework, GraphQL, AWS. Co-Founder, Polygon Guild Jaipur, organized largest Web3 event in Rajasthan, Polygon RoadShow Jaipur, 300+ attendees. Ayush leads impactful backend, DevOps projects, optimizes data processing, reduces downtime, improves API responsiveness. Holds B.Tech. in Information Technology from Manipal University Jaipur, received awards like ETH India 2022 Valist, Push Protocol Prize Pool. Ayush, Polygon Developer Advocate, volunteer, creator of projects like PoW'rd EthIndia 2022, brings valuable expertise to any team."},
-    { name: "Yarsh Garg", title: "Senior SDK Developer", photo: "/megabyte.jpg", description: ""},
+    { name: "Megabyte", title: "Senior SDK Developer", photo: "/megabyte.jpg", description: ""},
     { name: "Matt Davidson", title: "Dev Ops Engineer", photo: "/Matt.jpg", description: ""},
 ]
