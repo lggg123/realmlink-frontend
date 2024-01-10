0cc0e0776e8fff5d6628a9d0d4b95b7b1976d085
Jonny <Email@Email.com>
2024-01-10 05:33:59 +0000

    adding socials to footer

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 // components/Footer.tsx
 import React from 'react'
-
+import Link from 'next/link'
 const Footer: React.FC = () => {
   return (
     <footer className='bg-zinc-900 text-white py-6'>
@@ -8,27 +8,41 @@ const Footer: React.FC = () => {
         <p>© {new Date().getFullYear()} RealmLink. All rights reserved.</p>
         <p className='mt-2'>Follow us on social media:</p>
         <div className='flex justify-center mt-2 gap-4'>
-          <a
+          <Link
             href='https://twitter.com/RealmLink_Infra'
             target='_blank'
             rel='noopener noreferrer'
           >
             Twitter
-          </a>
-          <a
+          </Link>
+          <Link
             href='https://discord.gg/W3K47RnVAx'
             target='_blank'
             rel='noopener noreferrer'
           >
             Discord
-          </a>
-          <a
+          </Link>
+          <Link
             href='https://t.me/+BZqWjNfu9Bk2ZjZk'
             target='_blank'
             rel='noopener noreferrer'
           >
             Telegram
-          </a>
+          </Link>
+          <Link
+            href='https://www.instagram.com/realmlink_infra/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Instagram
+          </Link>
+          <Link
+            href='https://www.threads.net/@realmlink_infra'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Threads
+          </Link>
         </div>
       </div>
     </footer>
