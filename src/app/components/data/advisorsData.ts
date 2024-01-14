eaf6a2fbe3332b6122acc84c7993720642b246f8
George Lugo <machinelearner2334@gmail.com>
2024-01-13 18:04:05 -0800

    Update advisorsData.ts
    Removed some advisors that are not fully committed yet.

--- src/app/components/data/advisorsData.ts
+++ src/app/components/data/advisorsData.ts
@@ -4,11 +4,11 @@ export const advisorsData = [
     { name: "Dave Swinbank", title: "Strategic Advisor", photo: "/Dave.jpg", description: "Inspiring community builder | Forged Polygon Alliance into a blockchain beacon | Empathy + connections bridge the gap for all."},
     { name: "Steven Mckeon", title: "Security Advisor", photo: "/mac.jpg", description: "Ethical tech veteran | 25 years solving challenges, shaping blockchain's future | Hacks for good with FBI Infoguard."},
     { name: "Randall Miller", title: "Creative Advisor", photo: "/randall.jpg", description: 'Versatile multi-hyphenate | Films ("Bottle Shock", "Cotton Mary", "Coffee Wars") | Directing & crafting cinematic stories.'},
-    { name: "Taylor Spears", title: "Strategic Advisor", photo: "/taylor.jpg", description: "FinTech academic & leader | MSc at Edinburgh, risk management at Schulenburg Capital | Modelling, strategy, valuation - finance + tech + policy."},
+    // { name: "Taylor Spears", title: "Strategic Advisor", photo: "/taylor.jpg", description: "FinTech academic & leader | MSc at Edinburgh, risk management at Schulenburg Capital | Modelling, strategy, valuation - finance + tech + policy."},
     { name: "Zeshan Ahmed", title: "Strategic Advisor", photo: "/Zeshan.jpg", description: "Global tech architect | Blockchain strategist (Nature of Commerce) | Building seamless experiences across industries."},
     { name: "Januka Peiris", title: "Data Advisor", photo: "/januka.jpg", description: "Data-driven strategist | Transforms numbers into growth for thriving companies."},
     { name: "Christopher Race", title: "Financial Advisor", photo: "/Christopher.jpg", description: "Strategy & finance maestro | Growing businesses & minds at Orange Umbrella & RGE & Co."},
-    { name: "Philip Mendoca", title: "Strategic Advisor", photo: "/philip.png", description: "Experienced business adviser and founder, excels in turnarounds, finance, and CEO mentorship."},
+    // { name: "Philip Mendoca", title: "Strategic Advisor", photo: "/philip.png", description: "Experienced business adviser and founder, excels in turnarounds, finance, and CEO mentorship."},
     { name: "Bruno Miranda", title: "Community Advisor", photo: "/Bruno.jpg", description: "Web3 pioneer | Lisbon's Chilibangs founder, crafting impactful experiences."},
     { name: "Michael Harding", title: "Partnerships Advisor", photo: "/michael.jpg", description: "Creative tech leader | Bridging Web3 & AI to empower lives (Blockchain Sensei)."},
 ]
