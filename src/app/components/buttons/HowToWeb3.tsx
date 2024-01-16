ce0e7430b4481134dd515c9cf3e1a0ae001b8a6a
George Lugo <machinelearner2334@gmail.com>
2024-01-16 11:46:48 -0800

    Update HowToWeb3.tsx
    Created How To Web3 button.

---
+++ src/app/components/buttons/HowToWeb3.tsx
@@ -0,0 +1,19 @@
+// HowToWeb3.tsx
+import React from 'react';
+
+const HowToWeb3Button: React.FC = () => {
+    return (
+        <>
+            <a 
+                href="path/to/howtoweb3"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="brand-green-dark py-3 px-6 rounded-md shadow-md flex items-center justify-center"
+            >
+            How To Web3?
+            <a/>
+        </>
+    );
+}
+
+export default HowToWeb3Button;
