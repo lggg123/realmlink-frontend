20c42c13c8574b421db78508b1ddc7135838203a
George Lugo <machinelearner2334@gmail.com>
2024-01-16 12:10:09 -0800

    Update HowToWeb3.tsx
    Fixed tailwind.

--- src/app/components/buttons/HowToWeb3.tsx
+++ src/app/components/buttons/HowToWeb3.tsx
@@ -8,7 +8,7 @@ const HowToWeb3Button: React.FC = () => {
                 href="path/to/howtoweb3"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="brand-green-dark py-3 px-6 rounded-md shadow-md flex items-center justify-center"
+                className="bg-brand-green-dark py-3 px-6 rounded-md shadow-md flex items-center justify-center"
             >
             How To Web3?
             </a>
