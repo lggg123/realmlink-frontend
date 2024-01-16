d1b51b8f5ee0d7d4e140c9a66bda1b035ae59802
George Lugo <machinelearner2334@gmail.com>
2024-01-16 11:42:18 -0800

    Update WhyWeb3.tsx
    Created dark green why web3 button.

---
+++ src/app/components/buttons/WhyWeb3.tsx
@@ -0,0 +1,19 @@
+// WhyWeb3.tsx
+import React from 'react';
+
+const WhyWeb3Button: React.FC = () => {
+    return (
+        <>
+            <a 
+                href="path/to/whyweb3"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="brand-green-dark py-3 px-6 rounded-md shadow-md flex items-center justify-center"
+            >
+            Why Web 3?
+            <a/>
+        </>
+    );
+}
+
+export default WhyWeb3Button;
