f2c4f20967fe181471c7ecb4a6f966dd3480a12f
George Lugo <machinelearner2334@gmail.com>
2024-01-16 12:26:43 -0800

    Update WhyWeb3.tsx

--- src/app/components/buttons/WhyWeb3.tsx
+++ src/app/components/buttons/WhyWeb3.tsx
@@ -8,7 +8,7 @@ const WhyWeb3Button: React.FC = () => {
                 href="path/to/whyweb3"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="bg-brand-green-dark py-3 px-6 rounded-md shadow-md flex items-center justify-center"
+                className="bg-brand-green-dark py-3 px-6 mb-4 rounded-md shadow-md flex items-center justify-center"
             >
             Why Web3?
             </a>
