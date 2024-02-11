507f50c90526de269185ee9260a6995121de5256
George Lugo <georgelugo@Georges-MBP.lan>
2024-02-11 13:55:39 -0800

    feat: added fade in animation and redesigned hero image

--- src/app/components/layout/Hero.tsx
+++ src/app/components/layout/Hero.tsx
@@ -1,31 +1,35 @@
 import React from 'react'
 import Link from 'next/link'
+import { motion } from 'framer-motion'
 
 const Hero: React.FC = () => {
   return (
-    <section className='relative bg-hero-pattern bg-cover bg-center bg-no-repeat text-white text-center pt-28 pb-28 rounded-md shadow-xl backdrop-brightness-50 backdrop-filter backdrop-blur-sm'>
-        <h1 className='text-5xl font-extrabold mb-6'>
-          Begin an amazing gaming adventure.
+    <motion.section 
+      className='relative bg-hero-pattern bg-cover bg-center bg-no-repeat text-white pt-28 pb-28 px-56 rounded-md shadow-xl backdrop-brightness-50 backdrop-filter backdrop-blur-sm'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+        <h1 className='text-5xl font-extrabold mb-6 ml-2'>
+          Bring Your Collectibles<br/> Across Realms
         </h1>
-        <p className='text-xl mb-4'>
+        <p className='text-xl mb-4 text-center'>
           Dive into diverse worlds, from mystical lands to futuristic
           battlefields.
         </p>
-        <p className='text-xl mb-4'>
-          Carry your champions, pets and treasures across realms, breaking the
-          barriers of traditional gaming.
+        <p className='text-xl mb-4 text-center'>
+          Carry your champions, pets and treasures across realms,
+          <br/>breaking the barriers of traditional gaming.
         </p>
-        <p className='text-xl mb-8'>
-          Experience gaming revolutionized with web3 - more control, true
-          ownership, and seamless experiences.
-        </p>
-        <Link
-          className='inline-block bg-blue-600 hover:bg-blue-700 text-white py-3 px-8 rounded-full text-lg font-medium cursor-pointer transition duration-300 ease-in-out transform hover:-translate-y-1 shadow-lg'
-          href='/airdrop'
-        >
-          Join the Revolution - Register for Airdrop
-        </Link>
-    </section>
+        <div className="text-center">
+          <Link
+            className='inline-block bg-blue-600 hover:bg-blue-700 text-white py-3 px-8 text-center align-center rounded-full text-lg font-medium cursor-pointer transition duration-300 ease-in-out transform hover:-translate-y-1 shadow-lg'
+            href='/airdrop'
+          >
+            Join the Revolution - Register for Airdrop
+          </Link>
+        </div>
+    </motion.section>
   )
 }
 
